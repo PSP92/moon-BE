3d4b35f6ce2855b06839bdee571e0d3597b12c9e
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-19 10:02:40 -0700

    data

--- controllers/moon.js
+++ controllers/moon.js
@@ -1,11 +1,12 @@
 const Moon = require("../models/moon");
 const { Router } = require("express");
 const router = Router();
-
+const seedData = require("../db/seedData.json")
 //index route
 router.get("/", async (req, res) => {
   res.json(await Moon.find({}));
 });
+
 
 //create route
 router.post("/", async (req, res) => {
@@ -22,5 +23,21 @@ router.delete("/:id", async (req, res) => {
   res.json(await Moon.findByIdAndRemove(req.params.id));
 });
 
+router.get("/seed", async (req, res) => {
+    // try block for catching errors
+    try {
+      // remove all places from database
+      await Moon.remove({});
+      // add the seed data to the database
+      await Moon.create(placeSeedData);
+      // get full list of places to confirm seeding worked
+      const moons = await Moon.find({});
+      // return full list of places as JSON
+      res.json(moons);
+    } catch (error) {
+      // return error as JSON with an error status
+      res.status(400).json(error);
+    }
+  });
 // EXPORT ROUTER
 module.exports = router;
