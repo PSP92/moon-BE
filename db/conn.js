54e402d78da5084749d88772ea661cdec1460aea
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-19 09:56:19 -0700

    fixed

--- db/conn.js
+++ db/conn.js
@@ -1,20 +1,50 @@
 ///////////////////////////
 // Environmental Variables
 ///////////////////////////
-require("dotenv").config();
+// require("dotenv").config();
 
 /////////////////////////////////////
 // MONGOOSE CONNECTION
 /////////////////////////////////////
-const { MONGODBURI } = process.env;
+// const mongoose = require("mongoose");
+// const  MONGODBURI  = process.env.MONGODBURI;
+// console.log(MONGODBURI)
+// const config = { useUnifiedTopology: true, useNewUrlParser: true };
+
+
+// mongoose.connect(MONGODBURI, config);
+// const DB = mongoose.connection;
+// DB.on("open", () => console.log("You are connected to Mongo"))
+//   .on("close", () => console.log("You are disconnected to Mongo"))
+//   .on("error", (err) => console.log(err));
+
+// module.exports = mongoose;
+
+
+
+
+// LOAD ENVIRONMENTAL VARIABLES
+require("dotenv").config();
+
+// IMPORT DEPENDENCIES
 const mongoose = require("mongoose");
+
+// PULL OUT ENVIRONMENTAL VARIABLE FROM PROCESS.ENV OBJECT
+const MONGODB_URI ="mongodb://localhost:27017/moonapi";
+
+// Optional Configuration Object to Remove Mongo Deprecation Warnings
 const config = { useUnifiedTopology: true, useNewUrlParser: true };
-const DB = mongoose.connection;
 
-mongoose.connect(MONGODBURI, config);
+//Establish Connection to Database
+mongoose.connect(MONGODB_URI, config);
 
-DB.on("open", () => console.log("You are connected to Mongo"))
-  .on("close", () => console.log("You are disconnected to Mongo"))
-  .on("error", (err) => console.log(err));
+// Create Database Connection message for Open, Close, Error
+mongoose.connection
+  .on("open", () => console.log("MONGO CONNECTION IS OPEN"))
+  .on("close", () => console.log("MONGO CONNECTION IS CLOSED"))
+  .on("error", (error) =>
+    console.log("MONGO CONNECTION ERROR \n***************\n", error)
+  );
 
+// EXPORT MONGOOSE CONNECTION TO USE IN SERVER.JS
 module.exports = mongoose;
--- db/seed.js
+++ db/seed.js
@@ -1,38 +1,17 @@
-[{
-    img: "https://lh3.googleusercontent.com/proxy/vyNhuZNoqgHPxBrQ9y-VEKLoh-LEV0VWRhhJqKpX_Y838csHyJzDnVcRtMpFH_uENBP5ynFizLKUT2pLY3c8m62EqiWvnvdpkPhS4cJW0C-l-ckuV-bzvgedN7l3KEAqTHErey2jctrF5huLWz7W8SNUv0g0N0QR",
-        name: "Sailor Moon",
-        sign: "Cancer",
-        description: "Usagi is a careless young girl with an enormous capacity for love, compassion, and understanding. Usagi transforms into the heroine called Sailor Moon, Soldier of Love and Justice",
-},
-{
-    img: "https://i.pinimg.com/originals/3c/61/33/3c6133831bd6105545daf2878d7f2bc0.jpg",
-        name: "Tuxedo Mask",
-        sign: "Leo",
-        description: "As a young child he experienced a terrible car-accident that robbed him of his parents and of his knowledge of his own identity.His alter ego is Tuxedo Kamen, a man wearing a tuxedo and a mask. When Sailor Moon and her friends were in danger, he could sense it and would go to rescue them with a rose. ",
-},
-{
-    img: "https://i.pinimg.com/originals/5b/52/b2/5b52b2988d306d71945bb69a9867fb2e.jpg",
-    name: "Sailor Pluto",
-    sign: "Scorpio",
-    description: "Soldier of Time and Space, who has the duty of guarding the time corridor from unauthorized travelers.She has a distant personality and can be very stern, but can also be quite friendly and helps the Guardian Senshi when she can. ",
-},
-{
-    img: "https://www.vippng.com/png/detail/227-2277846_sailor-neptune-sailor-neptune-sailor-moon.png",
-        name: "Sailor Neptune",
-        sign: "Pisces",
-        description: "Soldier of Ocean and Intuition.She is fully devoted to this duty and willing to make any sacrifice for it. Sailor Neptune's talisman is her Deep Aqua Mirror, which aids her with her intuition and revealing cloaked evil.",
-},
-{
-    img: "https://i.pinimg.com/originals/1b/69/1a/1b691a53d0459d21dd8f5ca2e1f7a325.png",
-        name: "Sailor Uranus",
-        sign: "Aquarius",
-        description: "Soldier of the Sky and Fury.Sailor Uranus's talisman is known as the Space Sword, which aids her with her fighting and attacks.",
-},
-{
-    img: "https://pbs.twimg.com/media/EeWtYoxXsAI5PQo.jpg",
-        name: "Sailor Saturn",
-        sign: "Capricorn",
-        description: "After overcoming the darkness that has surrounded her family, Hotaru is able to become the Soldier of Death and Rebirth, Sailor Saturn. Sailor Saturn's weapon is her Silence Glaive, which aids her with her power to generate barriers and the power to destroy a planet.",
-}
-    
-]
+// import the db connection
+const mongoose = require('./conn')
+// import the model
+const Moon = require('../models/moon')
+
+
+// open a db connection
+const db = mongoose.connection
+// import seedData.json
+const manyMoon = require('./seedData.json')
+
+Moon.deleteMany({}).then(() => {
+	Moon.insertMany(manyMoon).then((moon) => {
+		console.log('our moon', moon);
+		db.close();
+	});
+});
